f30c6863f1300fcd271b6c761194eee7cbcf0b47
Pulkit Mehta <pulkitm404@gmail.com>
2020-03-21 19:40:09 +0530

    Updates

---
+++ precache-manifest.17ad6b00ce345d9dd3ef59f2f7527ec6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14fcc4ffc4cfe01a5f33584f742bffa5",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "987cb6fffa867bf47ea0",
+    "url": "/react-test/static/css/main.9df8a5fe.chunk.css"
+  },
+  {
+    "revision": "d93476ee820ac6f46d9f",
+    "url": "/react-test/static/js/2.6c63d74a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-test/static/js/2.6c63d74a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987cb6fffa867bf47ea0",
+    "url": "/react-test/static/js/main.afddbdd2.chunk.js"
+  },
+  {
+    "revision": "b8e8d19b8a6a5c4bec1b",
+    "url": "/react-test/static/js/runtime-main.f8b21e1c.js"
+  },
+  {
+    "revision": "a132f99bf2a0c79f05788d0f0a877ca6",
+    "url": "/react-test/static/media/saif-partners.a132f99b.jpg"
+  }
+]);
